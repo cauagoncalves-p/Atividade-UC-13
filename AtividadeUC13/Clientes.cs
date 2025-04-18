68cf45af8c6545888f73cf431fcac628a7b7d65f
cauagoncalves-p <cauagoncalves2190@gmail.com>
2025-04-18 20:11:05 -0300

    colocando o banco no forms

--- AtividadeUC13/Clientes.cs
+++ AtividadeUC13/Clientes.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AtividadeUC13.DubraSkateShopDataSetTableAdapters;
 
 namespace AtividadeUC13
 {
     public partial class Clientes: Form
     {
+
+        private void atualizarBanco() 
+        {
+            ClienteTableAdapter cliente = new ClienteTableAdapter();
+            var dados = from linha in cliente.GetData() select linha;
+
+            foreach (var item in dados)
+            {
+                lboDadosClientes.Items.Add(item);
+            }
+
+        }
         public Clientes()
         {
             InitializeComponent();
+            atualizarBanco();   
         }
     }
 }
